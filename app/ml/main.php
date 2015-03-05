29968428e2b9c14d5fbad34ff97123b56fbfe9dc
kelaocai <kelaocai@163.com>
2015-03-06 02:25:23 +0800

    debug

--- app/ml/main.php
+++ app/ml/main.php
@@ -24,7 +24,7 @@ class main extends AWS_CONTROLLER
 {
     public function get_access_rule()
     {
-        $rule_action['rule_type'] = 'black';
+        $rule_action['rule_type'] = 'white';
         $rule_action['actions'] = array();
 
         return $rule_action;
